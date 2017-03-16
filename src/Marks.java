c0e3bcfa1ade219e58faf164a11dd5d2b1590e3f
vasudev <vasu123689@gmail.com>
2017-03-15 18:48:18 -0800

    Aggregation

---
+++ src/Marks.java
@@ -0,0 +1,65 @@
+
+public class Marks {
+
+	private float telugu;
+	private float english;
+	private float maths;
+	private float hindi;
+	
+	
+		Marks(float telugu,float english,float maths,float hindi)
+		{
+		this.telugu=telugu;
+		this.english=english;
+		this.maths=maths;
+		this.hindi=hindi;
+		
+		}
+	
+	
+
+		public void SetTelugu(float telugu)
+		{
+		this.telugu=telugu;
+		}
+
+		public float getTelugu()
+		{
+			return telugu;
+		}
+	
+		
+		public void SetEnglish(float english)
+		{
+			this.english=english;
+		}
+
+		public float getEnglish()
+		{
+			return english;
+		}
+		
+		
+		public void SetMaths(float maths)
+		{
+				
+			this.maths=maths;
+		}
+
+		public float getMaths()
+		{
+			return telugu;
+		}
+			
+		public void SetHindi(float hindi)
+		{
+			this.hindi=hindi;
+		}
+
+		public float getHindi()
+		{
+			return telugu;
+		}
+					
+	
+}
---
+++ src/Student.java
@@ -0,0 +1,75 @@
+
+public class Student {
+	
+	private String name;
+	private String grade;
+	private char section;
+
+	Marks mkr;
+
+	Student(String name,String grade,char section, Marks mrk)
+	    {
+		this.name=name;
+		this.grade=grade;
+		this.section=section;
+		this.mkr=mrk;
+			
+		}
+	
+	
+
+	public void SetName(String name)
+		{
+		this.name=name;
+		}
+
+	
+	public String getName()
+		{
+		return name;
+		}
+
+
+	public void setGrade(String grade)
+		{
+		this.grade=grade;
+		}
+
+	public String getGrade()
+		{
+		return grade;
+		}
+	
+	public void SetSection(char section)
+		{
+		this.section=section;	
+		}
+	
+	public char getSection()
+		{
+		return section;
+		}
+	
+	
+		
+	public static void main(String args[])
+		{
+	       Marks mr = new Marks(85,87,82,91);
+	       Student s1 = new Student("Vasudev","11th",'A',mr);
+	       
+	       System.out.println("********Student Details********");
+	       
+	       System.out.println("Student Name:"+" "+s1.getName());
+	       System.out.println("Student Grade:"+" "+s1.getGrade());
+	       System.out.println("Student Section:"+" "+s1.getSection());
+	       
+	       System.out.println("*****Student Marks In Each Subject Out of 100*****");
+	       System.out.println("Hindi:"+" "+s1.mkr.getHindi());
+	       System.out.println("English:"+" "+s1.mkr.getEnglish());
+	       System.out.println("Maths:"+" "+s1.mkr.getMaths());
+	       System.out.println("Telugu"+" "+s1.mkr.getTelugu());
+		}
+	
+
+
+}
